cdee90c07c9a126f0be9591de20a904398bb2ab3
tomm-rodion <autovolna@gmail.com>
2023-12-16 02:39:20 +0200

    corection styled code

--- src/components/Register/YourActivity/YourActivity.jsx
+++ src/components/Register/YourActivity/YourActivity.jsx
@@ -1,12 +1,12 @@
-import { useFormik } from "formik";
-import { DivYourActivity } from "./YourActivity.styled";
+import { useFormik } from 'formik';
+import { DivYourActivity } from './YourActivity.styled';
 
 export const YourActivity = ({ onBack }) => {
   const formik = useFormik({
     initialValues: {
-      activity: "",
+      activity: '',
     },
-    onSubmit: (values) => {
+    onSubmit: values => {
       alert(JSON.stringify(values, null, 2));
       console.log(formik.values);
     },
@@ -24,73 +24,70 @@ export const YourActivity = ({ onBack }) => {
             To correctly calculate calorie and water intake
           </p>
         </div>
-        <form className="Form" onSubmit={formik.handleSubmit}>
-          <div className="DivRadioButton">
-            <label className="LabelActivity">
-              <input
-                className="Input"
-                id="1.2"
-                name="activity"
-                type="radio"
-                onChange={formik.handleChange}
-                value="1.2 - if you do not have physical activity and sedentary work"
-              />
-              1.2 - if you do not have physical activity and sedentary work
-            </label>
-            <label className="LabelActivity">
-              <input
-                className="Input"
-                id="1.375"
-                name="activity"
-                type="radio"
-                onChange={formik.handleChange}
-                value="1.375 - if you do short runs or light gymnastics 1-3 times a week"
-              />
-              1.375 - if you do short runs or light gymnastics 1-3 times a week
-            </label>
+        <form className="FormRadioButton" onSubmit={formik.handleSubmit}>
+          <label className="LabelActivity">
+            <input
+              className="Input"
+              id="1.2"
+              name="activity"
+              type="radio"
+              onChange={formik.handleChange}
+              value="1.2 - if you do not have physical activity and sedentary work"
+            />
+            1.2 - if you do not have physical activity and sedentary work
+          </label>
+          <label className="LabelActivity">
+            <input
+              className="Input"
+              id="1.375"
+              name="activity"
+              type="radio"
+              onChange={formik.handleChange}
+              value="1.375 - if you do short runs or light gymnastics 1-3 times a week"
+            />
+            1.375 - if you do short runs or light gymnastics 1-3 times a week
+          </label>
 
-            <label className="LabelActivity">
-              <input
-                className="Input"
-                id="1.55"
-                name="activity"
-                type="radio"
-                onChange={formik.handleChange}
-                value="1.55 - if you play sports with average loads 3-5 times a week"
-              />
-              1.55 - if you play sports with average loads 3-5 times a week
-            </label>
+          <label className="LabelActivity">
+            <input
+              className="Input"
+              id="1.55"
+              name="activity"
+              type="radio"
+              onChange={formik.handleChange}
+              value="1.55 - if you play sports with average loads 3-5 times a week"
+            />
+            1.55 - if you play sports with average loads 3-5 times a week
+          </label>
 
-            <label className="LabelActivity">
-              <input
-                className="Input"
-                id="1.725"
-                name="activity"
-                type="radio"
-                onChange={formik.handleChange}
-                value="1.725 ​​- if you train fully 6-7 times a week"
-              />
-              1.725 ​​- if you train fully 6-7 times a week
-            </label>
+          <label className="LabelActivity">
+            <input
+              className="Input"
+              id="1.725"
+              name="activity"
+              type="radio"
+              onChange={formik.handleChange}
+              value="1.725 ​​- if you train fully 6-7 times a week"
+            />
+            1.725 ​​- if you train fully 6-7 times a week
+          </label>
 
-            <label className="LabelActivity LabelLast">
-              <input
-                className="Input"
-                id="1.9"
-                name="activity"
-                type="radio"
-                onChange={formik.handleChange}
-                value="1.9 - if your work is related to physical labor, you train 2 times a day and include strength exercises in your training program"
-              />
-              1.9 - if your work is related to physical labor, you train 2 times
-              a day and include strength exercises in your training program
-            </label>
-          </div>
-          <div className="DivButtonSingUp">
-            <button className="ButtonSingUp" type="submit">
-              Sing Up
-            </button>
-          </div>
+          <label className="LabelActivity">
+            <input
+              style={{ width: '22px' }}
+              className="Input"
+              id="1.9"
+              name="activity"
+              type="radio"
+              onChange={formik.handleChange}
+              value="1.9 - if your work is related to physical labor, you train 2 times a day and include strength exercises in your training program"
+            />
+            1.9 - if your work is related to physical labor, you train 2 times a
+            day and include strength exercises in your training program
+          </label>
+          <button className="ButtonSingUp" type="submit">
+            Sing Up
+          </button>
         </form>
 
         <div className="DivButtonBack">
