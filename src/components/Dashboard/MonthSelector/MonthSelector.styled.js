43ec47b036182d354c07c993a514a050a7461af9
AnastasiiaMarchuk <stasijushka5@gmail.com>
2023-12-23 14:59:20 +0200

    transition

--- src/components/Dashboard/MonthSelector/MonthSelector.styled.js
+++ src/components/Dashboard/MonthSelector/MonthSelector.styled.js
@@ -1,27 +1,23 @@
 import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
+import { styled } from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  justify-content: space-between;
   align-items: center;
   padding: 16px 0;
 
   @media screen and (min-width: 834px) {
-    padding-top: 24px;
+    padding: 24px 0 16px;
   }
 
-  @media screen and (min-width: 1440px) {
-    padding-top: 20px;
+  div {
+    position: relative;
+    display: flex;
   }
-`;
-
-export const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
 
   button {
+    min-width: 140px;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -40,59 +36,43 @@ export const Wrapper = styled.div`
       font-size: 30px;
       line-height: 1.2;
     }
-  }
-  svg {
-    width: 16px;
-    height: 16px;
-    stroke: ${props => props.theme.color.primary_white};
-    transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
-  }
-  &:hover {
     svg {
-      stroke: ${props => props.theme.color.primary_green_lite};
+      width: 16px;
+      height: 16px;
+      stroke: ${props => props.theme.color.primary_white};
+      transition: all 350ms cubic-bezier(0.4, 0, 0.2, 1);
+      transform: rotate(0deg);
     }
-  }
-  &:focus {
-    svg {
-      stroke: ${props => props.theme.color.primary_green_lite};
+    &:hover {
+      svg {
+        stroke: ${props => props.theme.color.primary_green_lite};
+      }
+    }
+    &:focus {
+      svg {
+        stroke: ${props => props.theme.color.primary_green_lite};
+        transform: rotate(-180deg);
+      }
     }
   }
 
-  ul {
+  h2 {
     display: flex;
-    flex-direction: column;
-    padding-top: 12px;
-    gap: 6px;
-    width: 212px;
-    height: 144px;
-    border-radius: 12px;
-    background: ${props => props.theme.color.primary_black_2};
-    overflow-y: auto;
-    box-shadow: 0px 4px 14px 0px rgba(227, 255, 168, 0.2);
-    transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
-    position: absolute;
-    z-index: 1;
-
-    &::-webkit-scrollbar {
-      display: none;
-    }
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 1.4;
   }
 
   li {
-    font-weight: 400;
-    line-height: 20px;
-    cursor: pointer;
-  }
-
-  p {
     color: ${props => props.theme.color.primary_grey};
     font-size: 14px;
     font-style: normal;
-    font-weight: 400;
-    line-height: 1.4;
     text-align: start;
     width: 100%;
     padding-left: 16px;
+    font-weight: 400;
+    line-height: 1.4;
+    transition: all 0.3s cubic-bezier(0.4, 0, 0.2, 1);
 
     &:hover {
       background: rgba(255, 255, 255, 0.03);
@@ -105,11 +85,58 @@ export const Wrapper = styled.div`
     }
   }
 
-  h2 {
+  .list {
     display: flex;
-    font-size: 16px;
-    font-weight: 500;
-    line-height: 1.4;
+    flex-direction: column;
+    padding-top: 12px;
+    margin-top: 14px;
+    gap: 6px;
+    width: 212px;
+    height: 144px;
+    border-radius: 12px;
+    background: ${props => props.theme.color.primary_black_2};
+    box-shadow: 0px 4px 14px 0px rgba(227, 255, 168, 0.2);
+    overflow-y: auto;
+    position: absolute;
+    opacity: 1;
+    transform-origin: left top;
+    transform: scaleX(0) scaleY(0);
+
+    @media screen and (min-width: 834px) {
+      margin-top: 16px;
+    }
+  }
+
+  .select .list {
+    height: 0px;
+    top: 25%;
+    z-index: -1;
+    visibility: hidden;
+    opacity: 0;
+    transition: all 0.3s cubic-bezier(0.4, 0, 0.2, 1);
+  }
+
+  .select .open {
+    height: 144px;
+    top: 50%;
+    z-index: 1;
+    visibility: visible;
+    opacity: 1;
+    transform: scaleX(1) scaleY(1);
+    transition: all 0.3s cubic-bezier(0.4, 0, 0.2, 1);
+  }
+
+  *::-webkit-scrollbar {
+    display: none;
+  }
+
+  * {
+    scrollbar-width: thin;
+    scrollbar-color: transparent transparent;
+  }
+
+  *::-moz-scrollbar {
+    display: none;
   }
 `;
 
